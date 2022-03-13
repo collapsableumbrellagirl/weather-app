00533491209b2fd5520e756a52c75bb98b71e441
Veronica Bueno <52137475+vernbueno@users.noreply.github.com>
2022-03-13 11:37:35 -0500

    Added new weather icon and mapped to openweather ID

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 //initilize city display
-let city = "St Louis";
+let city = "Austin";
 let unit = "imperial";
 let apiKey = "87b9752c714fbde6317ef3900b3d8fb6";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=${unit}&appid=${apiKey}`;
@@ -30,6 +30,64 @@ const months = [
   "Dec",
 ];
 
+const newWeatherDayIcon = {
+  200: "Cloud angled rain zap.png",
+  201: "Cloud angled rain zap.png",
+  202: "Cloud angled rain zap.png",
+  210: "Sun cloud Zap.png",
+  211: "Sun cloud Zap.png",
+  212: "Cloud 3 Zap.png",
+  221: "Fast winds zaps.png",
+  230: "Cloud angled rain zap.png",
+  231: "Cloud angled rain zap.png",
+  232: "Cloud angled rain zap.png",
+  300: "Sun cloud little rain.png",
+  301: "Sun cloud little rain.png",
+  302: "Sun cloud angled rain.png",
+  310: "Sun cloud angled rain.png",
+  311: "Sun cloud angled rain.png",
+  312: "Sun cloud angled rain.png",
+  313: "Sun cloud angled rain.png",
+  314: "Sun cloud angled rain.png",
+  321: "Sun cloud angled rain.png",
+  500: "Sun cloud little rain.png",
+  501: "Sun cloud mid rain.png",
+  502: "Sun cloud angled rain.png",
+  503: "Sun cloud big rain.png",
+  504: "Sun cloud angled rain.png",
+  511: "Sun cloud hailstone",
+  520: "Cloud angled rain.png",
+  521: "Cloud angled rain.png",
+  522: "Cloud angled rain.png",
+  531: "Cloud angled rain.png",
+  600: "Sun cloud little snow.png",
+  601: "Sun cloud mid snow.png",
+  602: "Big snow little snow.png",
+  611: "Cloud hailstone.png",
+  612: "Sun cloud hailstone.png",
+  613: "Cloud hailstone.png",
+  615: "Sun cloud hailstone.png",
+  616: "Big snow little snow.png",
+  620: "Sun cloud little snow.png",
+  621: "Sun cloud mid snow.png",
+  622: "Mid snow fast winds.png",
+  701: "Sun slow wind.png",
+  711: "Sun fast wind.png",
+  721: "Sun cloud slow wind.png",
+  731: "Sun fast wind.png",
+  741: "Cloud slow wind.png",
+  751: "Sun fast wind.png",
+  761: "Sun cloud fast wind.png",
+  762: "Cloud fast wind.png",
+  771: "Fast winds zaps.png",
+  781: "Tornado.png",
+  800: "Sun.png",
+  801: "Sun cloud.png",
+  802: "Cloud.png",
+  803: "Cloud.png",
+  804: "Cloud.png",
+};
+
 /**
  * This function translates weather id codes from openweather to asset paths
  * for their icons because I think the icons from the site are ugly.
@@ -38,7 +96,9 @@ const months = [
  *
  * @returns str - the icon filename associated with the weather id code
  */
-function replaceIcon(id) {}
+function replaceIcon(id) {
+  return newWeatherDayIcon[id];
+}
 
 //format time for correct display
 function formatZeroPrefix(num) {
@@ -47,7 +107,7 @@ function formatZeroPrefix(num) {
 
 /**data transformation of timestamp for date and time display
  * Grabbing the timestamp from javascript to transform it for current date
- * @returns str - this keeps code single responsibility, transform that timestamp to string and displays
+ * @return str - this keeps code single responsibility, transform that timestamp to string and displays
  */
 function formatDate(timestamp) {
   let date = new Date(timestamp);
@@ -90,7 +150,7 @@ function displayWeatherOverview(response) {
     .querySelector("#weather-icon")
     .setAttribute(
       "src",
-      `Weatherly - Original render copy$/${replaceIcon(
+      `Weatherly - Original render copy/${replaceIcon(
         response.data.weather[0].id
       )}`
     );
@@ -98,3 +158,5 @@ function displayWeatherOverview(response) {
 
 //initializer function
 axios.get(apiUrl).then(displayWeatherOverview);
+
+console.log(apiUrl);
